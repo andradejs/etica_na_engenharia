ce7def075bf1beeeb26d90707c1892f81eeea706
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-02-19 01:15:13 -0300

    sera?

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
-
 const app = express();
 
+const { createServer } = require('@vercel/node');
 
 app.use(express.static('public'));
 
