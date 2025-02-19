6cae9fcd52436c79722aba33d4145625820c00d2
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-02-19 00:30:07 -0300

    ta feito

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'public','home.html'))
 })
 
+app.get("/sobre",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public','sobre.html'))
+})
+
+app.get("/impactossociais",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public','impactossociais.html'))
+})
+
+app.get("/privacidade",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public','privacidade.html'))
+})
+
+app.get("/responsabilidade",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public','responsabilidade.html'))
+})
 
 app.listen(PORT,()=>{
     console.log('Servidor rodando em http://localhost:3000')
